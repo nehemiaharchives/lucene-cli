6bde8df01e58684aaf2105371006f636422bf067
Hokuto Joel Ide <nehemiaharchive@gmail.com>
2026-07-12 18:36:58 +0900

    Use Runtime processor count on Native

--- src/nativeMain/kotlin/org/gnit/lucenekmp/cli/WorkerDefaults.native.kt
+++ src/nativeMain/kotlin/org/gnit/lucenekmp/cli/WorkerDefaults.native.kt
@@ -1,7 +1,3 @@
-@file:OptIn(kotlin.experimental.ExperimentalNativeApi::class)
-
 package org.gnit.lucenekmp.cli
 
-import kotlin.native.Platform
-
-internal actual fun availableProcessorCount(): Int = Platform.getAvailableProcessors()
+internal actual fun availableProcessorCount(): Int = Runtime.getRuntime().availableProcessors()
